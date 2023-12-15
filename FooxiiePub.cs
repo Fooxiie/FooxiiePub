4fb3e4b84abdb1b29aa28a56c82331d35a9ce47a
Fox <dev.fooxiie@gmail.com>
2023-12-15 20:51:10 +0100

    Correction de l'auto création des fichiers

--- FooxiiePub.cs
+++ FooxiiePub.cs
@@ -20,13 +20,13 @@ namespace FooxiiePub
     public class FooxiiePub : Plugin
     {
         public static double PricePub { get; private set; }
-
-        public static List<string> ForbiddenWord { get; private set; }
         public static string PubName { get; private set; }
         public static string TitleNotif { get; private set; }
         public static string MessageNotif { get; private set; }
 
-        public FooxiiePub(IGameAPI api) : base(api) { }
+        public FooxiiePub(IGameAPI api) : base(api)
+        {
+        }
 
         public override void OnPluginInit()
         {
@@ -34,10 +34,20 @@ namespace FooxiiePub
 
             var configFilePath = Path.Combine(pluginsPath, "FooxiiePub/config.json");
 
+            if (!Directory.Exists(Path.GetDirectoryName(configFilePath)))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(configFilePath) ??
+                                          Path.Combine(pluginsPath, "FooxiiePub"));
+            }
+
+            if (!File.Exists(configFilePath))
+            {
+                File.WriteAllText(configFilePath, "{\n    \"PricePub\": 1000,\n    \"PubName\": \"PUB\",\n    \"TitleNotif\": \"Pub\",\n    \"MessageNotif\": \"Une nouvelle pub est disponible par SMS\"\n}");
+            }
+
             Config configuration = ChargerConfiguration(configFilePath);
 
             PricePub = configuration.PricePub;
-            ForbiddenWord = configuration.ForbiddenWord;
             PubName = configuration.PubName;
             TitleNotif = configuration.TitleNotif;
             MessageNotif = configuration.MessageNotif;
@@ -54,77 +64,74 @@ namespace FooxiiePub
         private static void SetupCommand()
         {
             SChatCommand sChatCommand = new SChatCommand("/pub",
-                            "Créer une campagne de pub par SMS",
-                            "/pub", (Action<Player, string[]>)((player, arg) =>
+                "Créer une campagne de pub par SMS",
+                "/pub", (Action<Player, string[]>)((player, arg) =>
+                {
+                    string name = "";
+                    bool needPhone = false;
+
+                    UIPanel messagePanel = new UIPanel($"Message de la pub", UIPanel.PanelType.Input)
+                        .AddButton("Fermer", (ui) => { player.ClosePanel(ui); })
+                        .AddButton($"Payer ({PricePub}€)", (ui) =>
+                        {
+                            string message = ui.inputText;
+
+                            if (message != null && message.Length > 4 && message.Length < 300)
                             {
-                                string name = "";
-                                bool needPhone = false;
+                                if (player.character.Money >= PricePub)
+                                {
+                                    player.SendText("<color=#6aa84f>Pub commandé !</color>");
+                                    player.AddMoney(-PricePub, "Achat PUB SMS");
+                                    SendSMS(name, message, needPhone, player.character.PhoneNumber);
+                                }
+                                else
+                                {
+                                    player.SendText("<color=#fb4039>Vous n'avez pas assez d'argent.</color>");
+                                }
+                            }
+                            else
+                            {
+                                player.SendText(
+                                    "<color=#fb4039>Message de pub incorrect ! Plus de 5 caractères et moins de 300.</color>");
+                            }
 
-                                UIPanel messagePanel = new UIPanel($"Message de la pub", UIPanel.PanelType.Input)
-                                .AddButton("Fermer", (ui) =>
-                                {
-                                    player.ClosePanel(ui);
-                                })
-                                .AddButton($"Payer ({PricePub}€)", (ui) =>
-                                {
-                                    string message = ui.inputText;
+                            player.ClosePanel(ui);
+                        });
 
-                                    if (message != null && message.Length > 4 && message.Length < 300)
-                                    {
-                                        if (player.character.Money >= PricePub)
-                                        {
-                                            player.SendText("<color=#6aa84f>Pub commandé !</color>");
-                                            player.AddMoney(-PricePub, "Achat PUB SMS");
-                                            SendSMS(name, message, needPhone, player.character.PhoneNumber);
-                                        }
-                                        else
-                                        {
-                                            player.SendText("<color=#fb4039>Vous n'avez pas assez d'argent.</color>");
-                                        }
-                                    }
-                                    else
-                                    {
-                                        player.SendText("<color=#fb4039>Message de pub incorrect ! Plus de 5 caractères et moins de 300.</color>");
-                                    }
-                                    player.ClosePanel(ui);
-                                });
+                    UIPanel needPhoneNumberPanel =
+                        new UIPanel($"Ajouter le numéro de téléphone ? (Oui/Non)", UIPanel.PanelType.Text)
+                            .AddButton("Oui", (ui) =>
+                            {
+                                needPhone = true;
+                                player.ClosePanel(ui);
+                                player.ShowPanelUI(messagePanel);
+                            })
+                            .AddButton("Non", (ui) =>
+                            {
+                                needPhone = false;
+                                player.ClosePanel(ui);
+                                player.ShowPanelUI(messagePanel);
+                            });
 
-                                UIPanel needPhoneNumberPanel = new UIPanel($"Ajouter le numéro de téléphone ? (Oui/Non)", UIPanel.PanelType.Text)
-                                .AddButton("Oui", (ui) =>
-                                {
-                                    needPhone = true;
-                                    player.ClosePanel(ui);
-                                    player.ShowPanelUI(messagePanel);
-                                })
-                                .AddButton("Non", (ui) =>
-                                {
-                                    needPhone = false;
-                                    player.ClosePanel(ui);
-                                    player.ShowPanelUI(messagePanel);
-                                });
+                    UIPanel namePanel = new UIPanel($"Nom de l'envoyeur", UIPanel.PanelType.Input)
+                        .AddButton("Fermer", (ui) => { player.ClosePanel(ui); })
+                        .AddButton("Valider", (ui) =>
+                        {
+                            name = ui.inputText;
+                            if (name.Length < 3)
+                            {
+                                player.SendText("<color=#fb4039>Vous devez entrer un nom correct.</color>");
+                                player.ClosePanel(ui);
+                            }
+                            else
+                            {
+                                player.ClosePanel(ui);
+                                player.ShowPanelUI(needPhoneNumberPanel);
+                            }
+                        });
 
-                                UIPanel namePanel = new UIPanel($"Nom de l'envoyeur", UIPanel.PanelType.Input)
-                                .AddButton("Fermer", (ui) =>
-                                {
-                                    player.ClosePanel(ui);
-                                })
-                                .AddButton("Valider", (ui) =>
-                                {
-                                    name = ui.inputText;
-                                    if (name.Length < 3)
-                                    {
-                                        player.SendText("<color=#fb4039>Vous devez entrer un nom correct.</color>");
-                                        player.ClosePanel(ui);
-                                    }
-                                    else
-                                    {
-                                        player.ClosePanel(ui);
-                                        player.ShowPanelUI(needPhoneNumberPanel);
-                                    }
-                                });
-
-                                player.ShowPanelUI(namePanel);
-                            }));
+                    player.ShowPanelUI(namePanel);
+                }));
 
             sChatCommand.Register();
         }
@@ -132,17 +139,19 @@ namespace FooxiiePub
         private static async void SendSMS(string name, string message, bool needPhone, string fromtel)
         {
             List<Characters> list = await (from m in LifeDB.db.Table<Characters>()
-                                           where m.AccountId >= 0
-                                           select m).ToListAsync();
+                where m.AccountId >= 0
+                select m).ToListAsync();
             foreach (Characters character in list)
             {
                 if (needPhone)
                 {
-                    LifeDB.SendSMS(character.Id, PubName, character.PhoneNumber, Nova.UnixTimeNow(), $"[{name}]\n{message}\nTel:{fromtel}");
+                    LifeDB.SendSMS(character.Id, PubName, character.PhoneNumber, Nova.UnixTimeNow(),
+                        $"[{name}]\n{message}\nTel:{fromtel}");
                 }
                 else
                 {
-                    LifeDB.SendSMS(character.Id, PubName, character.PhoneNumber, Nova.UnixTimeNow(), $"[{name}]\n{message}");
+                    LifeDB.SendSMS(character.Id, PubName, character.PhoneNumber, Nova.UnixTimeNow(),
+                        $"[{name}]\n{message}");
                 }
             }
 
